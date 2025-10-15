aa641b91e07a665791c883c7f2aa51c4c0db221d
tale1 <tale1@DESKTOP-T5H8BTD>
2025-10-15 21:53:31 +0900

    java導入　初提出

---
+++ Hello world/src/sample/Selflntroduction.java
@@ -0,0 +1,23 @@
+package sample;
+
+public class Selflntroduction {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String name = "佐藤 謙太";
+	    int age = 30;
+	    String hobby = "読書";
+	    
+	    /*
+	     * 自己紹介文を出力する処理
+	     */
+	    
+	    System.out.println("私の名前は" + name + "です。");
+	    System.out.println("年齢は"+ age + "歳です。");
+	    System.out.println("趣味は"+ hobby +"です。");
+	    
+	    
+
+	}
+
+}
