42fe41c7a03d003175fb6c7640616b7891427e5a
tale1 <tale1@DESKTOP-T5H8BTD>
2025-11-06 19:45:13 +0900

    java導入　再提出

--- Hello world/src/sample/Selflntroduction.java
+++ Hello world/src/sample/Selflntroduction.java
@@ -13,8 +13,8 @@ public class Selflntroduction {
 	     */
 	    
 	    System.out.println("私の名前は" + name + "です。");
-	    System.out.println("年齢は"+ age + "歳です。");
-	    System.out.println("趣味は"+ hobby +"です。");
+	    System.out.println("年齢は" + age + "歳です。");
+	    System.out.println("趣味は" + hobby + "です。");
 	    
 	    
 
