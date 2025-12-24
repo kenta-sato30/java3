df953b3b61d4c2591f1dfccf234d2c4e003562ef
tale1 <tale1@DESKTOP-T5H8BTD>
2025-12-25 07:21:09 +0900

    メソッド～別クラスのインポート　初提出

---
+++ Hello world/src/curriculum_B/Animal.java
@@ -0,0 +1,26 @@
+package curriculum_B;
+
+public class Animal {//Q5の別クラス
+	private String name;
+	private double length;
+	private int speed;
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setLength(double length) {
+		this.length = length;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public double getLength() {
+		return this.length;
+	}
+	public int getSpeed() {
+		return this.speed;
+	}
+
+}
---
+++ Hello world/src/curriculum_B/Question5.java
@@ -0,0 +1,49 @@
+package curriculum_B;
+
+public class Question5 {
+
+	public static void helloWorld() {//Q1のメソッド
+		System.out.println("Hello, World!");
+		}
+	public static int doubleValue(int num) { //Q2のメソッド
+		return num * 2;
+		}
+	public static boolean isEven(int num) { //Q3のメソッド
+		return num % 2 == 0;
+		}
+	public static void main(String[] args) {
+		helloWorld();//Q1
+		System.out.println();
+		
+		int Value = 10;//Q2
+		int result =doubleValue(Value);
+		System.out.println( Value + "を2倍にすると" + result + "です。");
+		System.out.println();
+		
+		int num1 = 7;//Q3
+		int num2 = 10;
+		if (isEven(num2)) {
+			System.out.println(num2 + "は偶数です");
+		} else {
+			System.out.println(num2 + "は奇数です");
+		}
+		if (isEven(num1)) {
+			System.out.println(num1 + "は偶数です");
+		} else {
+			System.out.println(num1 + "は奇数です");
+		}
+		System.out.println();
+		
+		Greeting.sayHello();//Q4
+		System.out.println();
+		
+		Animal lion = new Animal();//Q5
+		lion.setName("ライオン");
+		lion.setLength(2.1);
+		lion.setSpeed(80);
+		System.out.println("動物名:" + lion.getName());
+		System.out.println("体長:" + lion.getLength());
+		System.out.println("速度:" + lion.getSpeed());
+	}
+
+}
