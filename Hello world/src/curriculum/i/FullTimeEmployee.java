a992a1565897ff49350377a29c58e4f34f692c0c
tale1 <tale1@DESKTOP-T5H8BTD>
2026-02-05 21:09:55 +0900

    継承　再提出

---
+++ Hello world/src/curriculum/i/FullTimeEmployee.java
@@ -0,0 +1,14 @@
+package curriculum.i;
+
+class FullTimeEmployee extends Employee {
+	public FullTimeEmployee(String id , String name) {
+		super(id, name);
+	}
+	@Override
+	public int calculateDailyWage(int hoursWorked) {
+		int houryRate = 1250;
+		int overtime = Math.max(0,hoursWorked - 8);
+		int regularHours = hoursWorked - overtime;
+		return(regularHours * houryRate) + (int)(overtime * houryRate * 1.25);
+	}
+}
