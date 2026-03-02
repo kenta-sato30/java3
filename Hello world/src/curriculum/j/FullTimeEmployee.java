8f6b735e5f7c2ba1a5b4914de2664e37ad001bce
tale1 <tale1@DESKTOP-T5H8BTD>
2026-03-02 21:05:26 +0900

    インターフェイスと抽象クラス　初提出

---
+++ Hello world/src/curriculum/j/FullTimeEmployee.java
@@ -0,0 +1,22 @@
+package curriculum.j;
+
+public class FullTimeEmployee extends Employee {
+	private static final int HOURLY_RATE = 1250;
+	private static final int STANDARD_HOURS = 8;
+	private static final double OVERTIME_MULTIPLIER = 1.25;
+
+	public FullTimeEmployee(String id , String name) {
+		super(id, name);
+	}
+	
+	@Override
+	public int costForDay(int hoursWorked) {
+		if (hoursWorked <= STANDARD_HOURS) {
+			return hoursWorked * HOURLY_RATE;
+		}
+		int Pay = STANDARD_HOURS * HOURLY_RATE;
+		int overHours = hoursWorked - STANDARD_HOURS;
+		int overPay = (int)(overHours * HOURLY_RATE * OVERTIME_MULTIPLIER);
+		return Pay + overPay;
+	}
+}
